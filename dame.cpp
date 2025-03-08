cf25b69ee38c7f305a00f32749c6add2ebbda652
arlr <arlrcode@gmail.com>
2025-03-08 18:01:25 +0100

    print the board

--- dame.cpp
+++ dame.cpp
@@ -7,7 +7,7 @@ class pion
 private:
     unsigned char x_coordinate = 0;
     unsigned char y_coordinate = 0;
-    enum color {WHITE, BLACK, DAME_WHITE, DAME_WHITE};
+    enum color {WHITE, BLACK, DAME_WHITE, DAME_BLACK};
     uint8_t ismove_possible(unsigned char target_x, unsigned char target_y);
     
 public:
@@ -24,66 +24,65 @@ pion::pion(unsigned char x, unsigned char y)
 
 }
 
-uint8_t pion::move(unsigned char target_x, unsigned char target_y)
+pion::~pion()
 {
-    // Test the move
-    uint8_t ret = 1; 
-    return ret;
 }
 
-uint8_t pion::ismove_possible(unsigned char target_x, unsigned char target_y)
-{
-    // Ruturn 1 if the move is possible
-    uint8_t ret = 0;
-    // Test if the target position is taken by an other piece
-
-    // Test if the target position is eating a adversary
-
-}
 class dame
 {
 private:
-    unsigned char board[8][8] = {{'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'}};
+unsigned char board[8][8] = {
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'},
+                            {'N','N','N','N','N','N','N','N'}
+                            };
+
     bool isplayer_white = false;
 
 
 public:
     dame(/* args */);
+    void print_board();
     ~dame();
 
 };
 
 dame::dame(/* args */)
 {
+    // Choose randomly the plyer color
+    if( rand() % 2){
+        isplayer_white = true;
+    }
+}
+
+void dame::print_board()
+{
+    for( uint8_t i = 0; i < 8; i++){
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << '\n';
+        
+    }
 }
 
 dame::~dame()
 {
 }
 
-pion::~pion()
-{
-}
 
 
 int main()
 {
-    unsigned char board[8][8] = {{'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'},
-                                {'N','N','N','N','N','N','N','N'}};
-    cout << "Dame game";
+    cout << "Dame game\n";
+    dame mygame;
+    mygame.print_board();
     return 0;
 }
 
